1f7192f8eb2aa6abc74e92e369699899028b9c20
C-Terminal <didactictraveller@skiff.org>
2023-05-29 18:43:19 -0400

    Default assets added and .gitignore

---
+++ Assets/Scripts/Dialogue/DialogueNode.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.Dialogue
+{
+
+    [System.Serializable]
+    public class DialogueNode
+    {
+
+        public string uniqueID, text;
+        public string[] children;
+
+    }
+}
