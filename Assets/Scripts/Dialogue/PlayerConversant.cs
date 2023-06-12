18054187cc16c3d2bc79c00b7a5d1bc6086b2e3a
C-Terminal <didactictraveller@skiff.org>
2023-06-12 16:31:35 -0400

    most recent changes

---
+++ Assets/Scripts/Dialogue/PlayerConversant.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace RPG.Dialogue
+{
+    public class PlayerConversant : MonoBehaviour
+    {
+        [SerializeField]
+        Dialogue currentDialogue;
+        
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
